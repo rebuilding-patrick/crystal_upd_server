aa50fe351dedfc7eb15bc7b47095d864391a9721
Solid Smoke <solid.smoke.software@gmail.com>
2019-12-30 15:24:46 -0800

    Working with game engine

--- network.cr
+++ network.cr
@@ -8,21 +8,36 @@ CONFIRM = "99"
 
 PLAYER_IDENT = "0"
 PLAYER_POS = "1"
-PLAYER_BLOCK = "2"
+PLAYER_CAST = "2"
 PLAYER_MSG = "3"
 PLAYER_ACTION = "4"
 PLAYER_LEVEL = "5"
+PLAYER_BLOCK_MOVE = "6"
+PLAYER_BLOCK_CLICK = "7"
+PLAYER_BLOCK_RELEASE = "8"
+PLAYER_JOIN_GAME = "9"
+PLAYER_START_GAME = "10"
 
+SERVER_GAME_LIST = "47"
+SERVER_GAME_JOIN = "48"
+SERVER_GAME_START = "49"
 SERVER_IDENT = "50"
 SERVER_POS = "51"
 SERVER_PING = "52" #This isn't being used due to constant communication. Remove?
 SERVER_LEVEL = "53" #This is new level information
-SERVER_BLOCK = "54" #This is block change information, usually from other players
+SERVER_BLOCK_DEL = "54" #This is block change information, usually from other players
 SERVER_NEW_ACTOR = "55"
-SERVER_DEL_ACTOR = "56"
+SERVER_DELETE = "56"
 SERVER_MSG = "57"
 SERVER_KICK = "58"
 SERVER_LEVEL_END = "59"
+SERVER_BLOCK_MOVE = "60"
+SERVER_NEW_TURN = "61"
+SERVER_CAST = "62"
+SERVER_BLOCK_CLICK = "63"
+SERVER_BLOCK_RELEASE = "64"
+SERVER_CAST_FAIL = "65"
+SERVER_HEALTH = "66"
 
 #eg b"1/150/100/join/0"
 # item[-1] 0 = time
@@ -35,6 +50,9 @@ struct Packet
 
   def initialize(@host, @data)
   end
+
+  def initialize(@data, @host)
+  end
 end
 
 
@@ -43,11 +61,10 @@ struct Message
   property data : String
 
   property time : Int64
-  property index : Int32
   property command : String
   property args : Array(String)
 
-  def initialize(@host, @data, @time, @index, @command, @args)
+  def initialize(@host, @data, @time, @command, @args)
   end
 end#struct
 
@@ -64,7 +81,7 @@ class MessagePool
     
     null_ip = Socket::IPAddress.new("0.0.0.0", 0)
     null_args = Array(String).new
-    null_msg = Message.new(null_ip, "null", -1_i64, 0, "null", null_args)
+    null_msg = Message.new(null_ip, "null", -1_i64, "null", null_args)
 
     size.times do |i|
       @pool << null_msg
@@ -102,24 +119,23 @@ class Parser
     @clock = clock
     @delimiter = "/"
     host = Socket::IPAddress.new "127.0.0.1", 0
-    @bad_message = Message.new(host, "bad message", 0_i64, 0, "-999", Array(String).new)
+    @bad_message = Message.new(host, "bad message", 0_i64, "-999", Array(String).new)
   end 
 
   def encode(command : String, data : String) : String
-    return "#{data}/#{command}/0/#{@clock.time}"
+    return "#{data}/#{command}/#{@clock.time}"
   end
 
-  def encode(command : String, data : String, index : Int32) : String
-    return "#{data}/#{command}#{index}#{clock.time}"
+  def encode(data : String) : String #TODO optional move this to the original message building if needed
+    return "#{data}/#{@clock.time}"
   end
 
   def decode(packet : Tuple(String, Socket::IPAddress)) : Message
     begin
       data = packet[0].split @delimiter
       time = data.pop.to_i64
-      index = data.pop.to_i32
       command = data.pop
-      return Message.new packet[1], packet[0], time, index, command, data
+      return Message.new packet[1], packet[0], time, command, data
     rescue err
       puts "Network: Error parsing packet #{packet[0]} from #{packet[1]} - #{err.message}"
       return @bad_message
@@ -209,164 +225,32 @@ class Network
 
   def recv : Message
     message = @parser.decode @socket.receive
-    if message.index > 0
-      if @resending.has_key? message.index
-        @resending.delete message.index
-      end
-    end
+    # if @resending.has_key? message.index
+    #   @resending.delete message.index
+    # end
     return message
   end
 
   def send(data : String, host : Socket::IPAddress)
     begin
       #puts "Server: Sending #{data} to #{host.address}:#{host.port}"
-      @socket.send data, host
+      @socket.send @parser.encode(data), host
     rescue err
       puts "Server: Error sending #{data} : #{err.message}"
     end
   end
 
-  def promise(command : String, data : String, host : Socket::IPAddress)
-    @index += 1
-    @resending[@index] = Packet.new host, @parser.encode(command, data, @index)
-  end
-
-  def send(command : String, data : String, host : Socket::IPAddress)
-    msg = @parser.encode command, data
-    send msg, host
-  end
-
   def sendall(data : String)
     #puts "Server: Sending all #{data}"
+    msg = @parser.encode(data)
     @connections.each_value do |con|
-      send data, con.host
+      send msg, con.host
     end
-  end
-
-  def sendall(command : String, data : String)
-    msg = @parser.encode command, data
-    sendall msg
   end
 
   def resend
     @resending.each_value do |packet|
       send packet.data, packet.host
-    end
-  end
-
-end#class
-
-
-class Server
-  property input : Messenger(Message)
-  property output : Messenger(Message)
-  property network : Network
-  property clock : Clock
-
-  def initialize
-    @input = Messenger(Message).new
-    @output = Messenger(Message).new
-    @clock = Clock.new 1000
-    @network = Network.new "127.0.0.1", 45456, @clock
-  end
-
-  def start
-    @server.bind
-
-    spawn do
-      while true
-        get_input
-        Fiber.yield
-      end
-    end
-
-    spawn do
-      while true
-        delta = @clock.tick
-        if delta > 0.0
-          handle_input 
-          handle_output
-        end
-        Fiber.yield
-      end
-    end
-  end
-
-  def get_input
-    packet = @network.recv
-    if packet
-      @input.send packet
-    end
-  end
-
-  def handle_input
-    packets = @input.get
-    packets.each do |packet|
-      @outgoing.send packet
-    end
-  end
-
-  def handle_output
-    packets = @output.get
-    packets.each do |packet|
-      @network.send packet
-    end
-  end
-
-end#class
-
-
-class Client
-  property input : Messenger(Message)
-  property output : Messenger(Message)
-  property network : Network
-  property clock : Clock
-
-  def initialize
-    @input = Messenger(Message).new
-    @output = Messenger(Message).new
-    @clock = Clock.new 1000
-    @network = Network.new "127.0.0.1", 45456, @clock
-  end
-
-  def start
-    spawn do
-      while true
-        get_input
-        Fiber.yield
-      end
-    end
-
-    spawn do
-      while true
-        delta = @clock.tick
-        if delta > 0.0
-          handle_input 
-          handle_output
-        end
-        Fiber.yield
-      end
-    end
-  end
-
-  def get_input
-    packet = @network.recv
-    if packet
-      @input.send packet
-    end
-  end
-
-  def handle_input
-    packets = @input.get
-    packets.each do |packet|
-      @outgoing.send packet
-    end
-  end
-
-  def handle_output
-    packets = @output.get
-    packets.each do |packet|
-      @network.send packet
     end
   end
 
